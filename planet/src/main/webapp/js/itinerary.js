6a1f737426755d9c4d0b35bae7e32d4228c3272a
Alice Zhou <alicezhou0805@gmail.com>
2020-06-26 17:03:25 +0000

    add events and delete events from itinerary page

---
+++ planet/src/main/webapp/js/itinerary.js
@@ -0,0 +1,55 @@
+
+function openForm() {
+    document.getElementById('add-event').style.display = 'block';
+}
+
+function closeForm() {
+    document.getElementById('add-event').style.display = 'none';
+}
+
+async function renderEvents() {
+    const eventsResponse = await fetch('/update-event');
+    const events = await eventsResponse.json();
+
+    events.forEach((event) => {
+        let eventElement = createEventElement (event.id, event.name, 
+            event.address, event.duration, event.preferredTime);
+        document.getElementById('events').appendChild(eventElement);
+    });
+}
+
+function createEventElement(id, name, address, duration, preferredTime) {
+    const eventElement = document.createElement('div');
+    eventElement.setAttribute("class", "card");
+    eventElement.innerHTML = 
+        `<div class="card-content">
+          <span class="card-title">` + name + `</span>
+          <p>` + address +`</p>
+        </div>
+        <div class="card-action">
+          <a>` + duration + ` hours </a>
+          <a>` + preferredTime + `</a>
+        </div>
+        <div>
+          <button onclick="deleteEvent(` + id + `)"> Delete </button>
+        </div>`;
+    return eventElement;
+}
+
+async function deleteEvent(id) {
+    await fetch('/update-event?id=' + id, {method: 'DELETE'});
+    window.location.reload(true); 
+}
+
+async function generateItinerary() {
+    // const eventsResponse = await fetch('/update-event');
+    // const events = await eventsResponse.json();
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('select');
+    let instances = M.FormSelect.init(elems, {});
+});
+
