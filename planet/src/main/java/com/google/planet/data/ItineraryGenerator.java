10842d20657ba3f16478314b0a7b10aaa183aa18
Alice Zhou <alicezhou0805@gmail.com>
2020-06-30 20:12:37 +0000

    delete itinerary generator function --will be in the next PR

--- planet/src/main/java/com/google/planet/data/ItineraryGenerator.java
+++ planet/src/main/java/com/google/planet/data/ItineraryGenerator.java
@@ -21,68 +21,6 @@ import java.util.ArrayList;
 public final class ItineraryGenerator {
     public List<ItineraryItem> generateItinerary(List<Event> events) { 
         List<ItineraryItem> items = new ArrayList<>();
-        boolean sessionIsFull = false;
-        int start_morning = TimeRange.getTimeInMinutes(8, 0);
-        int start_afternoon = TimeRange.getTimeInMinutes(12, 0);
-        int start_evening = TimeRange.getTimeInMinutes(16, 0);
-
-        for (Event event: events) {
-            // First, try to schedule the event in their preferred session
-            if (event.getPreferredTime().equals("8am - 12pm")) {
-                if (start_morning + event.getDurationInMinutes() > TimeRange.getTimeInMinutes(11, 59) + 1) {
-                    sessionIsFull = true;
-                }else {
-                    sessionIsFull = false; 
-                    ItineraryItem item = new ItineraryItem(event.getName(), event.getAddress(), 
-                        TimeRange.fromStartDuration(start_morning, event.getDurationInMinutes())); 
-                    items.add(item);
-                    start_morning += event.getDurationInMinutes();
-                }  
-            }else if (event.getPreferredTime().equals("12pm - 4pm")) {
-                if (start_afternoon + event.getDurationInMinutes() > TimeRange.getTimeInMinutes(15, 59) + 1) {
-                    sessionIsFull = true;
-                }else {
-                    sessionIsFull = false;
-                    ItineraryItem item = new ItineraryItem(event.getName(), event.getAddress(), 
-                        TimeRange.fromStartDuration(start_afternoon, event.getDurationInMinutes()));
-                    items.add(item);
-                    start_afternoon += event.getDurationInMinutes();
-                }  
-            }else if (event.getPreferredTime().equals("4pm - 8pm")) {
-                if (start_evening + event.getDurationInMinutes() > TimeRange.getTimeInMinutes(19, 59) + 1) {
-                    sessionIsFull = true;
-                }else {
-                    sessionIsFull = false;
-                    ItineraryItem item = new ItineraryItem(event.getName(), event.getAddress(), 
-                        TimeRange.fromStartDuration(start_evening, event.getDurationInMinutes()));
-                    items.add(item);
-                    start_evening += event.getDurationInMinutes();
-                }  
-            }
-
-            // If the preferred session is full, choose another session, check if the other session is available, then add
-            if (sessionIsFull){
-                if (start_morning + event.getDurationInMinutes() <= TimeRange.getTimeInMinutes(11, 59) + 1) {
-                    ItineraryItem item = new ItineraryItem(event.getName(), event.getAddress(), 
-                        TimeRange.fromStartDuration(start_morning, event.getDurationInMinutes()));
-                    items.add(item);
-                    start_morning += event.getDurationInMinutes();
-                }else if (start_afternoon + event.getDurationInMinutes() <= TimeRange.getTimeInMinutes(15, 59) + 1) {
-                    ItineraryItem item = new ItineraryItem(event.getName(), event.getAddress(), 
-                        TimeRange.fromStartDuration(start_afternoon, event.getDurationInMinutes()));
-                    items.add(item);
-                    start_afternoon += event.getDurationInMinutes();
-                }else if (start_evening + event.getDurationInMinutes() <= TimeRange.getTimeInMinutes(19, 59) + 1) {
-                    ItineraryItem item = new ItineraryItem(event.getName(), event.getAddress(), 
-                        TimeRange.fromStartDuration(start_evening, event.getDurationInMinutes()));
-                    items.add(item);
-                    start_evening += event.getDurationInMinutes();
-                }else {
-                    System.out.println("Too many events in one day!");
-                    return Arrays.asList();
-                }
-            }
-        }
         return items;
     }
 }
