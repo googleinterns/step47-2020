19c90ab59df5edba8aca58ec12d8e920030b0832
Emily Zhao <emily.zhao6@gmail.com>
2020-07-02 20:29:47 +0000

    Add preset search options as buttons

--- planet/src/main/webapp/places.js
+++ planet/src/main/webapp/places.js
@@ -12,23 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/** Declare global variables */
+let map;
+
 /** Initializes Map, implements search box and marks locations of searches */
 function initMap() {
     const TORONTO_COORDINATES = {lat:43.6532, lng:-79.3832}; 
-    let map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
         center: TORONTO_COORDINATES,
         zoom: 8
     });
-
-    let input = document.getElementById('pac-input');
-    let searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-        searchBox.setBounds(map.getBounds());
-    });
-
     let infoWindow = new google.maps.InfoWindow;
         // Gets users current position
         if (navigator.geolocation) {
@@ -48,13 +41,26 @@ function initMap() {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
+    updateSearch();
+}
+
+/** Create new markers and display new search */
+function updateSearch() {    
+    let input = document.getElementById('pac-input');
+    let searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
 
     let markers = [];
     let placeNames = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
+
+    // Listener for when user selects new location
     searchBox.addListener('places_changed', function() {
-        let places = searchBox.getPlaces();
+    let places = searchBox.getPlaces();
         if (places.length == 0) {
             return;
         }
@@ -102,39 +108,46 @@ function initMap() {
     });
 }
 
-/** List results of nearby search on page*/
-function listResults(results) {
-    const places = document.getElementById('places');
-
-    // Displays search results on page
-    for (let i = 0; i < results.length; i++) {
-        // Create materialize card for each result by creating dom element in HTML
-        let div1 = document.createElement("div");
-        div1.classList.add("card"); 
-        div1.classList.add("horizontal"); 
-
-        let div2 = document.createElement("div");
-        div2.classList.add("card-stacked");
-
-        let div3 = document.createElement("div");
-        div3.classList.add("card-content"); 
-
-        let p = document.createElement("p");
-        let content = document.createTextNode(results[i]); 
-        p.appendChild(content); 
-        div3.appendChild(p);
-        div2.appendChild(div3);
-        div1.appendChild(div2); 
-
-        let element = document.getElementById("results");
-        element.appendChild(div1);
-    }
-}
-
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
             'Error: The Geolocation service failed.' :
             'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
+}
+
+/** List results of nearby search on page*/
+function listResults(results) {
+    const places = document.getElementById('places');
+    let element = document.getElementById('results');
+    // Clear results 
+    element.innerHTML = ""; 
+
+    // Displays search results on page
+    for (let i = 0; i < results.length; i++) {
+        // Create materialize card for each result by creating dom element in HTML
+        let div1 = document.createElement('div');
+        div1.classList.add('card'); 
+        div1.classList.add('horizontal'); 
+
+        let div2 = document.createElement('div');
+        div2.classList.add("card-stacked");
+
+        let div3 = document.createElement('div');
+        div3.classList.add('card-content'); 
+
+        let p = document.createElement('p');
+        let content = document.createTextNode(results[i]); 
+        p.appendChild(content); 
+        div3.appendChild(p);
+        div2.appendChild(div3);
+        div1.appendChild(div2); 
+        element.appendChild(div1);
+    }
+    // Add search keyword to header
+    document.getElementById('greeting').innerHTML = "Find a place: " + document.getElementById('pac-input').value;
+}
+
+function searchOption(search) {
+    document.getElementById('pac-input').value = search;
 }
