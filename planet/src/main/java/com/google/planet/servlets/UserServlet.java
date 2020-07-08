926fa5b46b9feffea7753fcd0f56f979a2e0482a
Nabil Dabouz <ndabouz@google.com>
2020-07-08 13:10:07 +0000

    Add the UserServlet to get open the profile page of the user

---
+++ planet/src/main/java/com/google/planet/servlets/UserServlet.java
@@ -0,0 +1,39 @@
+package com.google.planet.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+    private String userName;
+    private String userEmail;
+    private String userPhone;
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+        this.userName = request.getParameter("name");
+        this.userEmail = request.getParameter("email");
+        this.userPhone = request.getParameter("phone");
+        response.sendRedirect("/user");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<html>");
+        response.getWriter().println("<head>");
+        response.getWriter().println("</head>");
+        response.getWriter().println("<body>");
+        response.getWriter().println("<h2>Welcome to your profile " + this.userName + "</h2>");
+        response.getWriter().println("<h3>Your email adress: " + this.userEmail + "</h3>");
+        response.getWriter().println("<h3>Your phone number: " + this.userPhone + "</h3>");
+        response.getWriter().println("</body>");
+        response.getWriter().println("</html>");
+    }
+}
