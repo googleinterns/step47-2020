7f2154eb7efe19ca38be87e6dfa15f487bfd2edd
Nabil Dabouz <ndabouz@google.com>
2020-07-09 13:36:30 +0000

    Replace the html content with a request dispatcher to forward the request to profile.jsp

--- planet/src/main/java/com/google/planet/servlets/UserServlet.java
+++ planet/src/main/java/com/google/planet/servlets/UserServlet.java
@@ -2,6 +2,8 @@ package com.google.planet.servlets;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,32 +11,25 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/user")
 public class UserServlet extends HttpServlet {
-    private String userName;
-    private String userEmail;
-    private String userPhone;
+    static public String userName;
+    static public String userEmail;
+    static public String userPhone;
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
-        this.userName = request.getParameter("name");
-        this.userEmail = request.getParameter("email");
-        this.userPhone = request.getParameter("phone");
+        UserServlet.userName = request.getParameter("name");
+        UserServlet.userEmail = request.getParameter("email");
+        UserServlet.userPhone = request.getParameter("phone");
         response.sendRedirect("/user");
     }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+      throws IOException, ServletException {
         response.setContentType("text/html");
         // This part is for demos purposes. We will be using RequestDispatcher to forward the request to another JSP file
-        response.getWriter().println("<html>");
-        response.getWriter().println("<head>");
-        response.getWriter().println("</head>");
-        response.getWriter().println("<body>");
-        response.getWriter().println("<h2>Welcome to your profile " + this.userName + "</h2>");
-        response.getWriter().println("<h3>Your email adress: " + this.userEmail + "</h3>");
-        response.getWriter().println("<h3>Your phone number: " + this.userPhone + "</h3>");
-        response.getWriter().println("</body>");
-        response.getWriter().println("</html>");
+        RequestDispatcher reauestDispatcher = request.getRequestDispatcher("profile.jsp");
+        reauestDispatcher.include(request, response);
     }
 }
