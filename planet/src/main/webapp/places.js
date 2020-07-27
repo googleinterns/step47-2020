bc8d4f2f1c45f7a7acdad137fdfed111ff8d3eea
Emily Zhao <emily.zhao6@gmail.com>
2020-07-27 20:16:29 +0000

    Add timestamp as int value in database

--- planet/src/main/webapp/places.js
+++ planet/src/main/webapp/places.js
@@ -365,10 +365,12 @@ function savePlace(x) {
 
 /** Update database and add placeID when place is saved by user */
 function updateDatabase(placeID, userID) {
-    let time = new Date();
+    const date = new Date();
+    const time = -date.getTime();
+
     // Add placeID to current userID in user tree with negative timestamp
     // The negative timestamp is for firebase sorting purposes in ascending order
-    database.ref('users/' + userID +'/places').child(placeID).set((-time.getTime()).toString());
+    database.ref('users/' + userID +'/places').child(placeID).set(time);
 }
 
 /** Delete placeID from current user when place is unsaved by user */
