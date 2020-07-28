fe96c8f842de3df29b9e5c81a00e294bda4db6d7
Emily Zhao <emily.zhao6@gmail.com>
2020-07-28 15:02:54 +0000

    List results one at a time and fix code for query limit

--- planet/src/main/webapp/places.js
+++ planet/src/main/webapp/places.js
@@ -20,7 +20,6 @@ let input;
 let searchBox;
 let markers = [];
 let promises = [];
-let placeInfo = [];
 let bounds;
 let count = 0;
 let places;
@@ -162,18 +161,19 @@ function addPlaceDetails() {
     document.getElementById('results').innerHTML = ""; 
 
     // Place Details Requests can only handle a query of 10 requests
-    let splice = places.splice(0,10);
-
+    const limit = 10; 
+    console.log(places);
+    let splice = places.splice(0,limit);
     // For each place, get the icon, name and location.
     splice.forEach(function(place) {
         sendPlaceRequest(place);
     });
 
-    // Place Details API can call place requests in groups of 20, 40, 60, etc. by using tokens
-    // Allow for all place requests to be called
-    while (places.length > 9) {
-        splice = places.splice(0,10);    
-        // Call remaining 10 place requests after 5 seconds to wait after request query empties
+    // Check if all places have been displayed
+    while (places.length > 0) {
+        splice = places.splice(0,limit);    
+        // Call remaining place requests in groups of max 10
+        // Wait 5 seconds for request query to empty
         setTimeout(function () {
             splice.forEach(function(place) {
                 sendPlaceRequest(place);
@@ -184,10 +184,6 @@ function addPlaceDetails() {
 
 /** Create place requests */
 function sendPlaceRequest(place) {
-    if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-    }
     // Make request with fields
     var place = {
         placeId: place.place_id,
@@ -226,13 +222,8 @@ function callback(place, status) {
         if (place.url) {
             placeDetails['Website'] = place.url;
         }
-        placeInfo.push(placeDetails);
-    }
-    // List results when all callbacks are finished
-    count ++ 
-    if (count === 10) {
-        count = 0; 
-        listResults();
+        // List place when callback is finished
+        listResults(placeDetails);
     }
 }
 
@@ -270,95 +261,94 @@ function createMarkers(place) {
     });
 }
 
-/** List results of nearby search on page*/
-function listResults() {
+/** List result of searched place on page*/
+function listResults(placeInfo) {
     let element = document.getElementById('results');
-    // Displays search results on page
-    for (let i = 0; i < placeInfo.length; i++) {
-        // Create materialize card for each result by creating dom element in HTML
-        let div1 = document.createElement('div');
-        div1.classList.add('card'); 
-        div1.classList.add('horizontal'); 
-        let div2 = document.createElement('div');
-        div2.classList.add('card-stacked');
-        let div3 = document.createElement('div');
-        div3.classList.add('card-content'); 
-
-        // Create elements to add to HTML
-        let p = document.createElement('p');
-        let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
-        let p3 = document.createElement('p');
-        let p4 = document.createElement('p');
-        let p5 = document.createElement('p');
-        let p6 = document.createElement('p');
-        let a = document.createElement('a');
-
-        // Set variables for place details
-        let name = document.createTextNode(placeInfo[i]['Name']);
-        let rating = document.createTextNode('Rating: ' + placeInfo[i]['Rating']);
-        let address = document.createTextNode(placeInfo[i]['Address']);
-        let phoneNumber = document.createTextNode('Phone Number: ' + placeInfo[i]['Phone']);
-        let openingHours = document.createTextNode('Opening Hours: ' + placeInfo[i]['Hours']);
-        let open = document.createTextNode(placeInfo[i]['Opening']);
-        let close = document.createTextNode(placeInfo[i]['Closing']);
-        let img = document.createElement('img');
         
-        // Check if user is signed in
-        if (currentUser) {
-            // Display save icons
-            let icon = createIcon(placeInfo[i]['PlaceID']);
-            div3.append(icon);
-        }
+    // Create materialize card for each result by creating dom element in HTML
+    let div1 = document.createElement('div');
+    div1.classList.add('card'); 
+    div1.classList.add('horizontal'); 
+    let div2 = document.createElement('div');
+    div2.classList.add('card-stacked');
+    let div3 = document.createElement('div');
+    div3.classList.add('card-content'); 
 
-        // Check for missing details, otherwise display through HTML
-        p.appendChild(name);        
-        p.setAttribute('id','place-name');
-        div3.appendChild(p);
-        if (placeInfo[i]['Rating'] != '') {
-            p1.appendChild(rating);
-            p1.setAttribute('id','place-rating');
-            div3.appendChild(p1);
-        }     
-         if (placeInfo[i]['Photo'] != '') {
-            img.src = placeInfo[i]['Photo'];
-            div3.appendChild(img); 
-        }      
-        if (address != '') {
-            p2.appendChild(address);
-            p2.setAttribute('id','place-address');
-            div3.appendChild(p2);
-        }
-        if (placeInfo[i]['Phone'] != '') {
-            p3.appendChild(phoneNumber);
-            p3.setAttribute('id','phone-number');
-            div3.appendChild(p3);         
-        }
-        if (placeInfo[i]['Hours'] != '') {
-            p4.appendChild(openingHours); 
-            p4.setAttribute('id','opening-hours');   
-            div3.appendChild(p4);      
-        }
-        if (placeInfo[i]['Opening'] != '') {
-            p5.appendChild(open);
-            p5.setAttribute('id','openingTime');
-            div3.appendChild(p5);
-        }
-        if (placeInfo[i]['Closing'] != '') {
-            p6.appendChild(close);
-            p6.setAttribute('id','closingTime');
-            div3.appendChild(p6); 
-        }
-        if (placeInfo[i]['Website'] != '') {
-            a.appendChild(document.createTextNode('Website'));
-            a.href = placeInfo[i]['Website'];
-            a.title = 'More'; 
-            div3.appendChild(a);
-        }
-        div2.appendChild(div3);
-        div1.appendChild(div2); 
-        element.appendChild(div1);
+    // Create elements to add to HTML
+    let p = document.createElement('p');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    let p4 = document.createElement('p');
+    let p5 = document.createElement('p');
+    let p6 = document.createElement('p');
+    let a = document.createElement('a');
+
+    // Set variables for place details
+    let name = document.createTextNode(placeInfo['Name']);
+    let rating = document.createTextNode('Rating: ' + placeInfo['Rating']);
+    let address = document.createTextNode(placeInfo['Address']);
+    let phoneNumber = document.createTextNode('Phone Number: ' + placeInfo['Phone']);
+    let openingHours = document.createTextNode('Opening Hours: ' + placeInfo['Hours']);
+    let open = document.createTextNode(placeInfo['Opening']);
+    let close = document.createTextNode(placeInfo['Closing']);
+    let img = document.createElement('img');
+        
+    // Check if user is signed in
+    if (currentUser) {
+        // Display save icons
+        let icon = createIcon(placeInfo['PlaceID']);
+        div3.append(icon);
     }
+
+    // Check for missing details, otherwise display through HTML
+    p.appendChild(name);        
+    p.setAttribute('id','place-name');
+    div3.appendChild(p);
+    if (placeInfo['Rating'] != '') {
+        p1.appendChild(rating);
+        p1.setAttribute('id','place-rating');
+        div3.appendChild(p1);
+    }     
+    if (placeInfo['Photo'] != '') {
+        img.src = placeInfo['Photo'];
+        div3.appendChild(img); 
+    }      
+    if (address != '') {
+        p2.appendChild(address);
+        p2.setAttribute('id','place-address');
+        div3.appendChild(p2);
+    }
+    if (placeInfo['Phone'] != '') {
+        p3.appendChild(phoneNumber);
+        p3.setAttribute('id','phone-number');
+        div3.appendChild(p3);         
+    }
+    if (placeInfo['Hours'] != '') {
+        p4.appendChild(openingHours); 
+        p4.setAttribute('id','opening-hours');   
+        div3.appendChild(p4);      
+    }
+    if (placeInfo['Opening'] != '') {
+        p5.appendChild(open);
+        p5.setAttribute('id','openingTime');
+        div3.appendChild(p5);
+    }
+    if (placeInfo['Closing'] != '') {
+        p6.appendChild(close);
+        p6.setAttribute('id','closingTime');
+        div3.appendChild(p6); 
+    }
+    if (placeInfo['Website'] != '') {
+        a.appendChild(document.createTextNode('Website'));
+        a.href = placeInfo['Website'];
+        a.title = 'More'; 
+        div3.appendChild(a);
+    }
+    div2.appendChild(div3);
+    div1.appendChild(div2); 
+    element.appendChild(div1);
+    
     // Add search keyword to header
     document.getElementById('greeting').innerHTML = 'Find a location: ' + document.getElementById('pac-input').value;
 }
