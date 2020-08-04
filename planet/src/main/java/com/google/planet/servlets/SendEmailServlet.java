cef2c3cea7a3f402a7b45adc14fa603d60287dd6
Alice Zhou <alicezhou0805@gmail.com>
2020-08-04 23:15:52 +0000

    set up mail api example servlet

---
+++ planet/src/main/java/com/google/planet/servlets/SendEmailServlet.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.appengine.mail;
+
+// [START simple_includes]
+import java.io.IOException;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+// [END simple_includes]
+
+// [START multipart_includes]
+import java.io.InputStream;
+// import java.io.ReaderInputStream;
+// import java.io.StringReader;
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+//import javax.activation.DataHandler;
+import javax.mail.Multipart;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMultipart;
+// [END multipart_includes]
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class SendEmailServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+      sendMultipartMail();
+  }
+
+  private void sendMultipartMail() {
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props, null);
+
+    String msgBody = "...";
+
+    try {
+      Message msg = new MimeMessage(session);
+      msg.setFrom(new InternetAddress("alicezhou0805@gmail.com"));
+      msg.addRecipient(Message.RecipientType.TO,
+                       new InternetAddress("alicexyz@google.com"));
+      msg.setSubject("Your Example.com account has been activated");
+      msg.setText(msgBody);
+
+      // [START multipart_example]
+      String htmlBody = "";          // ...
+      byte[] attachmentData = null;  // ...
+      Multipart mp = new MimeMultipart();
+
+      MimeBodyPart htmlPart = new MimeBodyPart();
+      htmlPart.setContent(htmlBody, "text/html");
+      mp.addBodyPart(htmlPart);
+
+      MimeBodyPart attachment = new MimeBodyPart();
+      String itinerary = "hi";
+      InputStream attachmentDataStream = new ByteArrayInputStream(itinerary.getBytes());
+      //InputStream attachmentDataStream = new ByteArrayInputStream(attachmentData);
+      attachment.setFileName("manual.pdf");
+      attachment.setContent(attachmentDataStream, "application/pdf");
+      mp.addBodyPart(attachment);
+
+      msg.setContent(mp);
+      // [END multipart_example]
+
+      Transport.send(msg);
+
+    } catch (AddressException e) {
+      System.out.println(e.getMessage());
+    } catch (MessagingException e) {
+      // ...
+       System.out.println(e.getMessage());
+    } 
+    // catch (UnsupportedEncodingException e) {
+    //   // ...
+    //    System.out.println(e.getMessage());
+    // }
+  }
+}
