5e609b2938c1100d950c6d8eeab77cb1954a0c56
Nabil Dabouz <ndabouz@google.com>
2020-08-05 23:15:52 +0000

    Add the database constante

--- planet/src/main/webapp/js/profile-events-renderer.js
+++ planet/src/main/webapp/js/profile-events-renderer.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const database = firebase.database();
 let userId;
 let listIndex = 0;
 let eventsList = [];
