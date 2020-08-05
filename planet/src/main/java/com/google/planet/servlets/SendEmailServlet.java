5d5bc1ab927e14e823845a20691bef9cf91ffb1e
Alice Zhou <alicezhou0805@gmail.com>
2020-08-05 20:52:19 +0000

    write custom email recipient and email body

--- planet/src/main/java/com/google/planet/servlets/SendEmailServlet.java
+++ planet/src/main/java/com/google/planet/servlets/SendEmailServlet.java
@@ -35,50 +35,58 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
+import java.io.BufferedReader;
 
 @WebServlet("/send-itinerary-to-email")
 public class SendEmailServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("alicexyz@google.com", "Alice Zhou"));
-            //TODO: change this to the signed in user's email address
-            message.addRecipient(Message.RecipientType.TO,
-                            new InternetAddress("alicexyz@google.com"));
-            //TODO: add custom subject, html body, and attached file
-            message.setSubject("Your itinerary for Toronto trip");
-            Multipart multipartContent = new MimeMultipart();
+        BufferedReader br = request.getReader();
+        String userEmail = request.getParameterMap().get("email")[0];
+        String itineraryContent = "";
 
-            // Add email body
-            MimeBodyPart htmlPart = new MimeBodyPart();
-            String htmlBody = "<div>Hello! <h3> Thank you for using Planet.</h3> " +
-                "<h3>You can see a copy of your itinerary in the attachment below.</h3></div>";  
-            htmlPart.setContent(htmlBody, "text/html");
-            multipartContent.addBodyPart(htmlPart);
+        if (br != null) {
+            itineraryContent = br.readLine();
+            Properties props = new Properties();
+            Session session = Session.getDefaultInstance(props, null);
+            try {
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("alicexyz@google.com", "Alice Zhou"));
+                message.addRecipient(Message.RecipientType.TO,
+                                new InternetAddress(userEmail));
+                //TODO: add custom subject, html body, and attached file
+                message.setSubject("Your itinerary for Toronto trip");
+                Multipart multipartContent = new MimeMultipart();
 
-            // Add a pdf attachment of the itinerary
-            // TODO: change input to actual itinerary
-            MimeBodyPart attachment = new MimeBodyPart();
-            String itinerary = "your itinerary";
-            InputStream attachmentDataStream = new ByteArrayInputStream(itinerary.getBytes("UTF-16"));
-            attachment.setContent(attachmentDataStream, "text/plain");
-            Date date= new Date();
-            long time = date.getTime();
-            String fileName = "itinerary" + (Long.toString(time)) +".txt";
-            attachment.setFileName(fileName);
-            multipartContent.addBodyPart(attachment);
+                // Add email body
+                MimeBodyPart htmlPart = new MimeBodyPart();
+                String htmlBody = "<div>Hello! <h3> Thank you for using Planet.</h3> " +
+                    "<h3>You can see a copy of your itinerary in the attachment below.</h3></div>";  
+                htmlPart.setContent(htmlBody, "text/html");
+                multipartContent.addBodyPart(htmlPart);
 
-            message.setContent(multipartContent);
-            Transport.send(message);
-        } catch (MessagingException e) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().println(e.getMessage());
-        } catch (UnsupportedEncodingException e) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().println(e.getMessage());
-        } 
+                // Add a pdf attachment of the itinerary
+                MimeBodyPart attachment = new MimeBodyPart();
+                InputStream attachmentDataStream = new ByteArrayInputStream(itineraryContent.getBytes("UTF-16"));
+                attachment.setContent(attachmentDataStream, "text/plain");
+                Date date= new Date();
+                long time = date.getTime();
+                String fileName = "itinerary" + (Long.toString(time)) +".txt";
+                attachment.setFileName(fileName);
+                multipartContent.addBodyPart(attachment);
+
+                message.setContent(multipartContent);
+                Transport.send(message);
+            } catch (MessagingException e) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getWriter().println(e.getMessage());
+            } catch (UnsupportedEncodingException e) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getWriter().println(e.getMessage());
+            } 
+        } else {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("");
+        }
     }
 }
